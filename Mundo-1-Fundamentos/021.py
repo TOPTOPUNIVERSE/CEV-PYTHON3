44ecf4848cc0cb9b1d985127c92d0aa12c0eb032
Luis Augusto <Laugusto370@gmail.com>
2021-09-04 16:51:23 -0300

    Tocando um MP3

---
+++ Mundo-1-Fundamentos/021.py
@@ -0,0 +1,10 @@
+"""
+   Desafio 021
+   Problema: Faça um programa em Python que abra e reproduza o áudio de um arquivo MP3."""
+
+import pygame
+pygame.mixer.init()
+pygame.mixer.music.load('shawn.mp3')
+pygame.mixer.music.play()
+input()
+pygame.event.wait()
