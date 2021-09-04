dd251abda1d04e3be393bf0409bc6f625e5ee441
Luis Augusto <Laugusto370@gmail.com>
2021-09-04 16:48:04 -0300

    Tabuada

---
+++ Mundo-1-Fundamentos/009.py
@@ -0,0 +1,23 @@
+"""
+Desafio 009
+Problema: Faça um programa que leia um número Inteiro qualquer
+          e mostre na tela a sua tabuada."""
+# Solicitando número para tabuada
+n = int(input('Mostre a tabuada do: '))
+# Mostrando na tela a tabuada do número inserido
+print('-'*15)
+print(f'\033[1;34m{n:2} x  1\033[m =\033[1;33m {n*1}\033[m')
+print(f'\033[1;34m{n:2} x  2\033[m =\033[1;33m {n*2}\033[m')
+print(f'\033[1;34m{n:2} x  2\033[m =\033[1;33m {n*2}\033[m')
+print(f'\033[1;34m{n:2} x  3\033[m =\033[1;33m {n*3}\033[m')
+print(f'\033[1;34m{n:2} x  4\033[m =\033[1;33m {n*4}\033[m')
+print(f'\033[1;34m{n:2} x  5\033[m =\033[1;33m {n*5}\033[m')
+print(f'\033[1;34m{n:2} x  6\033[m =\033[1;33m {n*6}\033[m')
+print(f'\033[1;34m{n:2} x  7\033[m =\033[1;33m {n*7}\033[m')
+print(f'\033[1;34m{n:2} x  8\033[m =\033[1;33m {n*8}\033[m')
+print(f'\033[1;34m{n:2} x  9\033[m =\033[1;33m {n*9}\033[m')
+print(f'\033[1;34m{n:2} x 10\033[m =\033[1;33m {n*10}\033[m')
+print('-'*15)
+#
+# Para cor azul: \033[1;34m
+# Para cor amarela: \033[1;33m
