35720198b2f79a0d1b3bba6c96da3698851ec856
Luis Augusto <Laugusto370@gmail.com>
2021-09-04 16:46:21 -0300

    Antecessor e sucessor

---
+++ Mundo-1-Fundamentos/005.py
@@ -0,0 +1,7 @@
+"""
+Desafio 005
+Problema: Faça um programa que leia um número inteiro e mostre na tela
+          o seu sucessor e antecessor."""
+
+n = int(input('Digite um número: '))
+print(f'O sucessor de {n} é {n + 1} \nO antecessor de {n} é {n - 1}')
