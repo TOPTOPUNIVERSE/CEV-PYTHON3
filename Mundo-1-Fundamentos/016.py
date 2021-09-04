f29f43950229980e637ec1082642f7396f46a535
Luis Augusto <Laugusto370@gmail.com>
2021-09-04 16:50:16 -0300

    Quebrando um número

---
+++ Mundo-1-Fundamentos/016.py
@@ -0,0 +1,8 @@
+"""
+Desafio 016
+Problema: Crie um programa que leia um número Real qualquer
+          pelo teclado e mostre na tela a sua porção Inteira."""
+
+
+n = float(input('Digite um valor:'))
+print(f'O número {n} tem a parte inteira {int(n)}')
