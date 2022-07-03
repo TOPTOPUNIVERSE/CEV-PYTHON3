c960eb14b748e48e70e589c54da2f4235c9140e5
Luis Augusto <Laugusto370@gmail.com>
2022-07-03 14:36:26 -0300

    Delete Mundo 2 directory

--- Mundo 2/M2D47.py
+++
@@ -1,4 +0,0 @@
-print('>'*10, 'Mostre os números pares', '<'*10) #Cabeçalho do programa
-for c in range (2, 51, 2):
-        print(c, end=' ')
-print('FIM')
--- Mundo 2/M2D48.py
+++
@@ -1,8 +0,0 @@
-print('-='*10, 'SOMA DE NUMEROS IMPARES MULTIPLOS DE 3', '-='*10)
-s = 0
-co = 0
-for c in range(1, 501, 2):
-    if c % 3 == 0:
-        co += 1 #co = co + 1
-        s += c #s = s + c
-print(f'A soma de todos os {co} valores solicitados é {s}')
