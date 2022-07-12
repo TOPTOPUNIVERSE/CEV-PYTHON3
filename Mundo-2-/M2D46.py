791693a82bea024a20c9b34c0d9ef078d9fb00b5
Luis Augusto <Laugusto370@gmail.com>
2022-07-12 09:18:19 -0300

    Delete Mundo-2- directory

--- Mundo-2-/M2D46.py
+++
@@ -1,9 +0,0 @@
-from time import sleep #Importando biblioteca de tempo
-print('-='*10, 'COUNTDOWN for the NEW YEAR', '-='*10) #Cabeçalho do programa
-#Laço para o countdown
-for c in range (10, 0, -1): #Do 10 ao 0 em ordem decrescente
-    sleep(1) #Espere 1 segundo
-    print(c) #Conte
-sleep(0.5)
-print('HAPPY NEW YEAR!!!') #Mostre na tela happy new year
-print('-='*10, 'END', '-='*10) #Cabeçalho de fim do programa
